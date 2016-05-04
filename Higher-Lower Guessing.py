bc2fd60c44c62fa7c325a279db86545842d36182
torogitar <torje.n.eikenes@gmail.com>
2016-05-04 11:27:00 +0200

    Add try again

--- Higher-Lower Guessing.py
+++ Higher-Lower Guessing.py
@@ -1,29 +1,31 @@
 from random import randint
 import sys
 
-number = randint(1,100)
+num = randint(1,100)
 
-def guess(tries):
+def guess(tries, number):
 	tries += 1
 	unum = int(raw_input())
 	if(unum > number):
 		print "Your number is too big"
-		guess(tries)
+		guess(tries, number)
 	elif(unum < number):
 		print "Your number it too small"
-		guess(tries)
+		guess(tries, number)
 	elif(unum == number):
 		print "You guessed the correct number!"
 		print "You used %i tries" % (tries)
+		end()
 	else:
 		print "Something went wrong"
 		guess(tries)
 
 def end():
-	print "Do you want to play again?"
+	print "Do you want to play again? (y/n)"
 	reply = raw_input()
 	if(reply.lower() == "y"):
-		guess(0)
+		print "Guess a number between 1 and 100"
+		guess(0, randint(1,100))
 	elif(reply.lower() == "n"):
 		sys.exit
 	else:
@@ -31,5 +33,5 @@ def end():
 		end()
 
 print "Guess a number between 1 and 100"
-guess(0)
+guess(0, num)
 
