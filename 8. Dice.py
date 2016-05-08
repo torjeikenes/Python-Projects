21590fb917d358075854085aa213377d2d0ff5bf
torogitar <torje.n.eikenes@gmail.com>
2016-05-08 20:47:44 +0200

    Add loop and percentage of occurance of number

--- 8. Dice.py
+++ 8. Dice.py
@@ -22,8 +22,24 @@ def dice():
     sides = getSides()
     throws = getThrows()
     results = []
+    done = []
     for i in range(0, throws):
         results.append(randint(1, sides))
     print results
+    print "after " + str(throws) + " throws. The following numbers were rolled: "
+    for i in results:
+        print i,
+    print "\n"
+    for i in results:
+        if i not in done: 
+            occurance = results.count(i)
+            print str(i) + " was rolled " + str(occurance) + " times. " + \
+            str(percentage(occurance, results)) + "% of all throws"
+            done.append(i)
+
+    dice()
+
+def percentage(occurance, resultList):
+    return 100 * float(occurance)/float(len(resultList))
 
 dice()
